b8417bee24e0fdcec899c5ad05db491db7734bc3
emmr <mpapico@gmail.com>
2017-06-21 09:22:39 +0000

    https://automatetheboringstuff.com/chapter3/ zeroDivide.py エラー処理(try, except)

--- 03_関数/zeroDivide.py
+++ 03_関数/zeroDivide.py
@@ -1,8 +1,11 @@
 def spam(divideBy):
-    return 42 / divideBy
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
 
 print(spam(2))
 print(spam(12))
-print(spam(0))
+print(spam(0))  # ERROR!
 print(spam(1))
 
