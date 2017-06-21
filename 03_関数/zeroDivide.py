5e161598c0a37bc6a20d5b17f1bf38efc52fbf3f
emmr <mpapico@gmail.com>
2017-06-21 09:26:07 +0000

    https://automatetheboringstuff.com/chapter3/ zeroDivide.py エラー処理を関数の外に出した場合

--- 03_関数/zeroDivide.py
+++ 03_関数/zeroDivide.py
@@ -1,11 +1,13 @@
 def spam(divideBy):
-    try:
-        return 42 / divideBy
-    except ZeroDivisionError:
-        print('Error: Invalid argument.')
+    return  42 / divideBy
+    
 
-print(spam(2))
-print(spam(12))
-print(spam(0))  # ERROR!
-print(spam(1))
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))  # ここで処理が止まる
+    print(spam(1))  # これは実施されない
+except ZeroDivisionError:
+    print('Error: Invalid argument.')
+
 
