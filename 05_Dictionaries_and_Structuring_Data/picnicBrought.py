0f440ea0f71ee2135603e74f61d87a956d184601
mimura <mimura@nhn-techorus.com>
2017-07-10 11:41:09 +0900

    https://automatetheboringstuff.com/chapter5/

---
+++ 05_Dictionaries_and_Structuring_Data/picnicBrought.py
@@ -0,0 +1,16 @@
+allGuests = {'Allice': {'apple': 5, 'pretzels': 12},
+             'Bob': {'ham sandwiches': 3, 'apples': 2},
+             'Carol': {'cups': 3, 'apple pies': 1}}
+
+def totalBrought(guests, item):
+    newBrought = 0
+    for k, v in guests.items():
+        newBrought = newBrought + v.get(item, 0)
+    return newBrought
+
+print('Number og things being broughe:')
+print(' - Apples        ' + str(totalBrought(allGuests, 'apple')))
+print(' - Cups          ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes         ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwiches' + str(totalBrought(allGuests, 'ham sandwiches')))
+print(' - Apple Pies    ' + str(totalBrought(allGuests, apple pies')))
