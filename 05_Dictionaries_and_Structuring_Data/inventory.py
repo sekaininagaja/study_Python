ff5e5763639ff79802b17b4b66a6a353a8f7e4f3
emmr <mpapico@gmail.com>
2017-07-10 06:52:00 +0000

    https://automatetheboringstuff.com/chapter5/ - inventory.py

--- 05_Dictionaries_and_Structuring_Data/inventory.py
+++ 05_Dictionaries_and_Structuring_Data/inventory.py
@@ -4,7 +4,8 @@ def displayInventory(inventory):
     print("Inventry:")
     item_total = 0
     for k, v in inventory.items():
-        item_total = v
+        print(str(stuff[k]) + '  ' + k )
+        item_total = item_total + v
     print("Total number if items: " + str(item_total) )
 
 displayInventory(stuff)
