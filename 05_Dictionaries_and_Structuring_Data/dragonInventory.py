82f283fbf38b0bd689adad83fd7033aa0928e19d
emmr <mpapico@gmail.com>
2017-07-27 08:21:14 +0000

    https://github.com/emmr/study_Python/blob/master/05_Dictionaries_and_Structuring_Data/05_Dictionaries_and_Structuring_Data.md - dragonInventory.py

--- 05_Dictionaries_and_Structuring_Data/dragonInventory.py
+++ 05_Dictionaries_and_Structuring_Data/dragonInventory.py
@@ -1,19 +1,30 @@
-def displayInventory(inventory):
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = {'gold coin': 42, 'rope': 1}
+
+# dragon_loot のアイテムがinv内に存在するか確認し、存在していたら+1する
+def add_to_inventory(inventory, added_items):
+    for i in range(len(added_items)):
+        item_name = added_items[i]
+        item_total = 0
+        if item_name in inventory:
+            item_total = int(inventory[item_name]) + 1
+            inventory[item_name] = item_total         
+        else:
+            inventory[item_name] = 1
+            #inventory.setdefault(item_name, 1)
+    i = i+1
+    return inventory
+
+def display_inventory(inventory):
     print("Inventry:")
     item_total = 0
     for k, v in inventory.items():
-        print(str(stuff[k]) + '  ' + k )
+        print(str(inventory[k]) + '  ' + k )
         item_total = item_total + v
     print("Total number if items: " + str(item_total) )
 
-def addToInventory(inventory, add):
-    item_total = 0
-    # dragonLoot のアイテムがinv内に存在するか確認
-    for i in range(len(dragonLoot)):
-        item_total = item_total + i.get(add, 0)
-    return item_total
+# for debug
+#import pdb;pdb.set_trace()
 
-inv = {'gorl coin': 42, 'rope': 1}
-dragonLoot = ['gold coin', 'dagger', 'gord coin', 'gold coin', 'ruby']
-inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
+inv = add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
