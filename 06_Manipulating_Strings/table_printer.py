dbc46ecfffa525a24dd9862bb24c0ab1662e5926
mimura <mimura@nhn-techorus.com>
2017-08-04 19:19:48 +0900

    add

---
+++ 06_Manipulating_Strings/table_printer.py
@@ -0,0 +1,10 @@
+#! Python
+
+table_data = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+def print_table(value) :
+    
+
+print_table(table_data)
