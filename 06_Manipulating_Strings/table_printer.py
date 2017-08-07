d980acbfe6cc8e4e7b99870fe45ee5e3118eb5bd
mimura <mimura@nhn-techorus.com>
2017-08-07 18:24:29 +0900

    途中

--- 06_Manipulating_Strings/table_printer.py
+++ 06_Manipulating_Strings/table_printer.py
@@ -5,6 +5,18 @@ table_data = [['apples', 'oranges', 'cherries', 'banana'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 def print_table(value) :
-    
+    col_widths = [0] * len(table_data)
+    x = 0
+    y = 0
+
+    for x in range(len(table_data[x])):
+        for y in range(len(table_data)):
+            print('===', y,x, '===')
+            if col_widths[y] < len(table_data[y][x]):
+                col_widths[y] = len(table_data[y][x])
+            print(col_widths[y])
+            y = y + 1
+#            col_widths[y] = len(table_data[y][x])
+        x = x + 1
 
 print_table(table_data)
