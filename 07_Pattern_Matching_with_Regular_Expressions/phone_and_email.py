11cb29a2949ba1967d7ca356601025ca48f33d3f
emmr <mpapico@gmail.com>
2017-08-31 02:39:32 +0000

    phone_and_email.py

---
+++ 07_Pattern_Matching_with_Regular_Expressions/phone_and_email.py
@@ -0,0 +1,41 @@
+#! python3
+# phone_and_email.py - Finds phone numbers and email addresses on the clipboard.
+
+import pyperclip, re
+
+# phone_number regex.
+phone_regex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?      # area core
+    (\s|-|\.)?              # separator
+    (\d{3})                 # first 3 digits
+    (\s|-|\.)               # separator
+    (\s\(ext|x|ext.\)\s*(\d{2,5}))? # extension
+    )''', re.VERBOSE)
+
+# email regex.
+email_regex = re.compile(r'''(
+    [a-zA-Z0-9._%=-]+       # username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z]{2,4})       # dot-something
+    )''', re.VERBOSE)
+
+# Find matches in clipboard text.
+text = str(pyoerclip.paste())
+matches = []
+for groups in phone_regex.findall(text):
+    phone_num = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phone_num += ' x' + groups[8]
+    matches.append(phone_num)
+for groups in email_regex.findall(text):
+    matches.append(groups[0])
+
+# Copy results to the clipboard.
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses foun.')
+
