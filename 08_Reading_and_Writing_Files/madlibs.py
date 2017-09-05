0c0e76b23898bb3b147170aa2637ece6493cd2aa
mimura <mimura@nhn-techorus.com>
2017-09-05 18:31:45 +0900

    途中

--- 08_Reading_and_Writing_Files/madlibs.py
+++ 08_Reading_and_Writing_Files/madlibs.py
@@ -1,10 +1,14 @@
 #! python3
 
-import re, os
+import re
+import os
+import sys
 
-template_file = open('hoge.txt', 'r')
+template_file = open('hoge.txt')
 template_file_content = template_file.read()
 
-print(template_file_content)
+placeholder_regex = re.compile(r'ADJECTIVE|NOUN|VERB|ADVERB')
+print(placeholder_regex.findall(template_file_content))
+mo = placeholder_regex.search(template_file_content)
 
 template_file.close()
