6b1b24ce4b181f3fe9b9f52cdd8244bb4baa1f1c
mimura <mimura@nhn-techorus.com>
2017-09-06 19:44:32 +0900

    途中

--- 08_Reading_and_Writing_Files/madlibs.py
+++ 08_Reading_and_Writing_Files/madlibs.py
@@ -1,14 +1,24 @@
 #! python3
 
-import re
-import os
-import sys
+import re, os, sys
 
 template_file = open('hoge.txt')
 template_file_content = template_file.read()
 
 placeholder_regex = re.compile(r'ADJECTIVE|NOUN|VERB|ADVERB')
-print(placeholder_regex.findall(template_file_content))
-mo = placeholder_regex.search(template_file_content)
+mo = placeholder_regex.findall(template_file_content)
+replacement_word = []
 
+for i in range(len(mo)):
+    if mo[i] == 'ADJECTIVE':
+        replacement_word = replacement_word + [input('Please input ADJECTIVE: ')]
+    elif mo[i] == 'NOUN':
+        replacement_word = replacement_word + [input('Please input NOUN: ')]
+    elif mo[i] == 'VERB':
+        replacement_word = replacement_word + [input('Please input VERB: ')]
+    elif mo[i] == 'ADVERB':
+        replacement_word = replacement_word + [input('Please input ADVERB: ')]
+    i += 1
+
+print(replacement_word)
 template_file.close()
