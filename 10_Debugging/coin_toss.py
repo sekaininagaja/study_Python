2331b6bf99373bd27032f55fd880a04a64390c2d
emmr <mpapico+github@gmail.com>
2017-09-11 21:50:52 +0900

    デバッグ後

--- 10_Debugging/coin_toss.py
+++ 10_Debugging/coin_toss.py
@@ -2,15 +2,22 @@
 
 import random
 guess = ''
+
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
+if toss == 1:
+    toss = 'heads'
+else:
+    toss = 'tails'
+
+
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
+    guess = input()
     if toss == guess:
        print('You got it!')
     else:
